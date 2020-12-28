f6e10e012a05d1f07d70a837703bc5b06e41d65f
Jakub Pierzchlewicz <jakubpierzchlewicz@hotmail.com>
2020-12-28 18:26:18 +0100

    Finished LinkedList ADT implementation

--- src/main/java/java15/playground/list/LinkedListUsage.java
+++ src/main/java/java15/playground/list/LinkedListUsage.java
@@ -1,6 +1,8 @@
 package java15.playground.list;
 
-public class LinkedListTest {
+import static java15.playground.list.LinkedList.*;
+
+public class LinkedListUsage {
     public static void main(String[] args) {
         LinkedList<String> emptyList = new LinkedList.Nil<>();
         System.out.println(emptyList);
@@ -10,8 +12,15 @@ public class LinkedListTest {
 
         LinkedList<Integer> list = new LinkedList.Nil<>();
         for (int i = 0; i < 10; i++) {
-            list = list.addElement(list, i);
+            list = addElement(list, i);
         }
         System.out.println(list);
+        System.out.println(length(list));
+        System.out.println(head(list));
+        System.out.println(tail(list));
+        System.out.println(init(list));
+        System.out.println(last(list));
+        System.out.println(append(tail(list), init(list)));
+        System.out.println(length(append(tail(list), init(list))));
     }
 }
